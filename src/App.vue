7e3769346aa0f3015e2dd78f6a4c7031522cdb93
liuhuihao <1061036802@qq.com>
2019-07-08 15:16:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+    <div ref="container" class="container">
+    </div>
+</template>
+
+<script>
+    import * as THREE from 'three';
+    import {MTLLoader, OBJLoader} from 'three-obj-mtl-loader';
+
+    const OrbitControls = require('three-orbit-controls')(THREE);
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                container: null,
+                scene: '',
+                light: '',
+                camera: '',
+                controls: '',
+                renderer: '',
+            }
+        },
+        methods: {
+            init() {
+                this.initScene();
+                this.initLight();
+                this.initCamera();
+                this.initControls();
+                this.initObj();
+            },
+
+            // 初始化画布
+            initScene() {
+                this.scene = new THREE.Scene();
+                this.container = this.$refs.container;
+                this.renderer = new THREE.WebGLRenderer({alpha: true});
+                this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+                this.container.appendChild(this.renderer.domElement);
+            },
+
+            // 初始化光源
+            initLight() {
+                this.scene.add(new THREE.AmbientLight('#FFFFFF')); // 环境光
+            },
+
+            // 初始化相机
+            initCamera() {
+                this.camera = new THREE.PerspectiveCamera(60, this.container.clientWidth / this.container.clientHeight, 10, 1000);
+                this.camera.position.set(0, 20, 100);
+            },
+
+            // 初始化控制器
+            initControls() {
+                this.controls = new OrbitControls(this.camera);
+                this.controls.target.set(0, 15, 0);
+                this.controls.update();
+            },
+
+            // 初始化模型
+            initObj() {
+                new MTLLoader().setPath('/static/male02/').load('male02.mtl', materials => {
+                    materials.preload();
+                    new OBJLoader().setMaterials(materials).setPath('/static/male02/').load('male02.obj', obj => {
+                        obj.scale.set(0.3, 0.3, 0.3);
+                        obj.position.set(0, 0, 0);
+                        this.scene.add(obj)
+                    })
+                });
+            },
+
+            // 渲染
+            animate() {
+                requestAnimationFrame(this.animate);
+                this.controls.update();
+                this.renderer.render(this.scene, this.camera);
+            },
+        },
+        mounted() {
+            this.init();
+            this.animate();
+        }
+    }
+</script>
+
+<style>
+    .container {
+        width: 500px;
+        height: 500px;
+        border: 1px solid black;
+    }
+</style>
